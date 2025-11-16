233db018d838dd7726843f62e304638aa2e89137
Rubén Benito Santos <rbenito@qaroni.com>
2025-11-16 23:14:22 +0100

    Favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Sara Velázquez | Portfolio')</title>
-    <link rel="icon" type="image/png" href="/logo.png">
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
 </head>
 
